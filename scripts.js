26a23704d79a177af5bcf13995bfe47ac50bc4c1
afrin <afrin.moni@gmail.com>
2020-04-03 12:16:15 +0600

    all

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-//start
+//function expression to select elements
+const selectelElement = (s) => Ducument.querySelector(s);
+//open the menu on click
+selectElement(s: '.open').addEventlistener('click',() =>{
+    selectElement(s:'.nab-list').classlist.add('active');
+
+});
+//close the menu on click
+selectElement(s: '.close').addEventlistener('click',() =>{
+    selectElement(s:'.nab-list').classlist.remove(tokens:'active');
+
+});
